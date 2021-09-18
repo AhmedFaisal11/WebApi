36baf98030eb76ea9992b586d319e492d6d196e5
Ahmedbinfaisal <67901435+AhmedFaisal11@users.noreply.github.com>
2021-09-18 18:57:07 +0500

    Add files via upload

---
+++ Data/DemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Data
+{
+    public class DemoDbContext : DbContext
+    {
+        public DemoDbContext(DbContextOptions<DemoDbContext> options):base(options){   }
+
+        public DbSet<Users> Users { get; set; }
+    }
+}
---
+++ Data/Users.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDemo.Data
+{
+    public class Users
+    {
+        [Key]
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+    }
+}
